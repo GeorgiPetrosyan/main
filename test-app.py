a1495ac392859a9c13c1c73b420a3d637829a5d9
GeorgiPetrosyan <gpetrosyan99@gmail.com>
2024-07-12 22:12:04 +0400

    Add files via upload

---
+++ test-app.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+
+    page.title = "Test app"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    user_ip = page.client_ip
+    result_text = ft.Text()
+
+    def button_click(event):
+        result_text.value = f"Here is your IP address: {user_ip}"
+        page.update()
+
+    button = ft.ElevatedButton(text="Get your IP", on_click=button_click)
+
+    page.add(
+        button,
+        result_text
+    )
+
+
+ft.app(target=main)
